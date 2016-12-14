943d84f1ae5b86a8f5c6c3576471860a74c8914b
ykjver <ykjver@gmail.com>
2016-12-15 04:08:31 +0800

    rename statics to static

--- summer/controllers/admin/User.php
+++ summer/controllers/admin/User.php
@@ -1,10 +1,18 @@
 <?php defined('BASEPATH') || exit('no direct script access allowed');
 
-class user extends Ykj_Controller {
+class User extends MY_Controller {
 
 	public function __construct(){
 		parent::__construct();
 
 		$this -> load -> model('user_model');
+	}
+
+	public function index() {
+		echo 'index';
+	}
+
+	public function login() {
+		echo 'admin login';
 	}
 }
